c35ff40072ebca1d08c8d6a758dca3f9f28b8d88
yazeed_alshehri <109772125+yazidalshehrii@users.noreply.github.com>
2023-02-12 02:18:43 +0300

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,68 @@
+#include <Servo.h>
+
+Servo myservo;  
+Servo myservo1; 
+int IR1=2;
+int IR2=4;
+int Nobusypark=0;
+
+int pos = 0;   
+void setup() {
+  myservo.attach(3);
+  myservo1.attach(5);  
+  
+  Serial.begin(115200);
+  pinMode(IR1, INPUT);
+  pinMode(IR2, INPUT);
+}
+
+void loop() {
+  int sensorStatus1 = digitalRead(IR1);
+  int sensorStatus2 = digitalRead(IR2);
+  if (sensorStatus1==1)
+  {
+  	if(Nobusypark<=5)
+  	{
+    	Nobusypark=Nobusypark+1;
+      
+    	for (pos = 180; pos >= 0; pos -= 1) 
+        { 
+    	myservo1.write(pos);             
+    	delay(15);        
+  		}
+    }
+    else if (Nobusypark>5)
+    {
+      for (pos = 0; pos <= 180; pos += 1) 
+      { 
+   		 myservo.write(pos);             
+    	 delay(15);                       
+  	  }
+      for (pos = 180; pos >= 0; pos -= 1)
+      { 
+    	myservo1.write(pos);             
+   		delay(15);        
+      }
+    }
+    else if(Nobusypark==0)
+    {
+         for (pos = 0; pos <= 180; pos += 1) 
+      { 
+   		 myservo1.write(pos);             
+    	 delay(15);                       
+  	  }    
+  	}
+    }
+  else if(sensorStatus2==1)
+  {
+    if(Nobusypark>0)
+    {
+    Nobusypark=Nobusypark-1;
+    for (pos = 180; pos >= 0; pos -= 1) 
+        { 
+    	myservo.write(pos);             
+    	delay(15);        
+  		}
+    }
+  }   
+}
